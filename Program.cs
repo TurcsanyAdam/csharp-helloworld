942440ce99d63917cc8060f8801eda374600aa3e
TurcsanyAdam <55133868+TurcsanyAdam@users.noreply.github.com>
2019-12-03 23:07:28 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 1)
+            {
+                foreach (string name in args)
+                {
+                    if (name != "Program.cs")
+                    {
+                        Console.WriteLine("Hello " + name + " !");
+                    }
+                    
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hello World !");
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
